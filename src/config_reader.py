0790f859fc21791aa4255e10f716071ea58ba47f
Kashish245 <nagarkashish2024@gmail.com>
2025-05-04 20:19:57 +0530

    Preprocessing and visualization of processed data are done

--- src/config_reader.py
+++ src/config_reader.py
@@ -1,9 +1,10 @@
 import yaml
 import os
+import sys
 from src.logger import logging
 from src.exception import CustomException
 
-def read_config(config_path: str):
+def read_config(config_path: str = "config.yaml") -> dict:
     try:
         with open(config_path, 'r') as file:
             config = yaml.safe_load(file)
